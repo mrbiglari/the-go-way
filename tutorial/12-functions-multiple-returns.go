d6b5d35e3494216478d794e2992545bbedbda818
mrbiglari <reza.biglari@sky.uk>
2025-02-24 12:19:14 +0000

    feat: Adding tutorial example for functions with multiple return values in Go.

---
+++ tutorial/12-functions-multiple-returns.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func getThree() (int, string) {
+	return 3, "three"
+}
+
+func functionsMultipleReturns() {
+	var threeInt, threeString = getThree()
+	fmt.Printf("Return values, '%d' and '%s' fetched from getThree() function invocation.", threeInt, threeString)
+}
