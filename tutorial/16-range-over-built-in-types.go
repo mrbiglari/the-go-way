c1283b487bd6d84f7383d253f635ad7259fcfde0
mrbiglari <reza.biglari@sky.uk>
2025-02-24 14:09:09 +0000

    feat: add tutorial example for range over built-in types in Go

---
+++ tutorial/16-range-over-built-in-types.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func rangeOverBuiltInTypes() {
+	numbers := []int{2, 3, 4}
+	for index, number := range numbers {
+		fmt.Printf("number[%d]:%d\n", index, number)
+	}
+
+	var aliases = map[string]string{"apollo13": "bob", "artemis": "sydney"}
+	for key, value := range aliases {
+		fmt.Printf("%s -> %s\n", key, value)
+	}
+
+	for key := range aliases {
+		fmt.Println("key:", key)
+	}
+
+	for index, character := range "go" {
+		fmt.Println(index, character)
+	}
+}
