a1d8b395c01f158c19bd0914d6682011de618326
mrbiglari <reza.biglari@sky.uk>
2025-02-24 17:11:23 +0000

    docs: Updating comments for pointer tutorial example in Go.

--- tutorial/17-pointers.go
+++ tutorial/17-pointers.go
@@ -6,8 +6,8 @@ func setZeroValueViaValue(variable int) {
 	variable = 0
 }
 
-func setZeroValueViaPointer(variable *int) {
-	*variable = 0
+func setZeroValueViaPointer(variable *int) { // `*` in `*int` defines the type of `variable` as a pointer to an int.
+	*variable = 0 // `*` in `*variable` dereferences the pointer to access the address location to read/write.
 }
 
 func pointers() {
@@ -15,9 +15,9 @@ func pointers() {
 	fmt.Printf("Value of 'x' is: %d\n", x)    // 3
 	fmt.Printf("Address of 'x' is: %d\n", &x) // 0x42131100
 
-	var y = &x                              // & operator gets `address of` `x` // type of `y` is `*int`
+	var y = &x                              // `&` operator gets `address of` `x` // type of `y` is `*int`
 	fmt.Printf("Value of 'y' is: %d\n", y)  // 0x42131100 // the value is the address of `x`.
-	fmt.Printf("Value of 'x' is: %d\n", *y) // 3 // * operator `dereferences` pointer `y` to get value of `x`.
+	fmt.Printf("Value of 'x' is: %d\n", *y) // 3 // `*` operator `dereferences` pointer `y` to get value of `x`.
 
 	*y = 10 // dereferencing `y` allows to update value of `x`
 	fmt.Printf("Value of 'x' is now: %d\n", x)
