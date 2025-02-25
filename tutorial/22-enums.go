909fda862dd8165cda1ab0700a8dcf3a079ba427
mrbiglari <reza.biglari@sky.uk>
2025-02-25 16:57:54 +0000

    feat: add tutorial example demonstrating enums in Go

---
+++ tutorial/22-enums.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type ServerState int
+
+const (
+	Idle ServerState = iota
+	Connected
+	Error
+)
+
+var stateToTextMapping = map[ServerState]string{
+	Idle:      "idle",
+	Connected: "connected",
+	Error:     "error",
+}
+
+func (v ServerState) String() string {
+	return stateToTextMapping[v]
+}
+
+func enums() {
+	var serverState = Idle
+
+	if serverState == Idle {
+		print(serverState)
+	}
+
+	var nextState = transition(serverState)
+	print(nextState)
+}
+
+func print(state ServerState) {
+	fmt.Printf("The server's status is '%v' (value: %d).\n", state, state)
+}
+
+func transition(state ServerState) ServerState {
+	switch state {
+	case Idle:
+		return Connected
+	case Connected:
+		return Idle
+	case Error:
+		return Error
+	default:
+		panic(fmt.Errorf("unknown state: %s", s))
+	}
+}
