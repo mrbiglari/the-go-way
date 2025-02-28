d7ea4bcab6e3b4022469aa5c1c5608ac2e30a456
mrbiglari <reza.biglari@sky.uk>
2025-02-28 15:46:57 +0000

    feat: add iterator examples in Go

---
+++ tutorial/27-range-over-iterators.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"iter"
+)
+
+type ChainList[T any] struct {
+	head *Chain[T]
+}
+
+type Chain[T any] struct {
+	value T
+	next  *Chain[T]
+}
+
+func (pointer *ChainList[T]) enumerate() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for i := pointer.head; i != nil; i = i.next {
+			if !yield(i.value) {
+				return
+			}
+		}
+	}
+}
+
+func fibonacciSequence() iter.Seq[int] {
+	return func(yield func(int) bool) {
+		var a, b = 1, 1
+		for {
+			if !yield(a) { // yield is an internal `iter` function that determines if the iteration should continue, true: continues and false: terminates.
+				return
+			}
+			a, b = b, a+b
+		}
+	}
+}
+
+func iterators() {
+	var list = &ChainList[int]{head: &Chain[int]{value: 1, next: &Chain[int]{value: 2, next: &Chain[int]{value: 3}}}}
+
+	for element := range list.enumerate() {
+		fmt.Println(element)
+	}
+
+	for n := range fibonacciSequence() {
+		if n > 15 {
+			break // when for is terminated (break or return), yield() returns false and terminates generating iteration items.
+		}
+		fmt.Println(n)
+	}
+}
