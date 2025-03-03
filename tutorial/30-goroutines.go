1e11fac1fed5a076a2b8a6f8db46d472cab864d4
mrbiglari <reza.biglari@sky.uk>
2025-03-03 19:11:35 +0000

    feat: add tutorial for goroutines with counting and interjection examples

---
+++ tutorial/30-goroutines.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func count() {
+	for i := 0; i < 5; i++ {
+		fmt.Printf("...%d\n", i)
+	}
+}
+
+func goRoutines() {
+	fmt.Println("Let's start counting!")
+
+	go count()
+
+	var interject = func() {
+		fmt.Println("No! I interject!")
+	}
+	go interject()
+
+	time.Sleep(time.Second)
+	fmt.Println("Counting finished!")
+}
