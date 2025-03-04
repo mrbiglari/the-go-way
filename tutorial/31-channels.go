0fb16b72f358e28328ba903c05dfafc24eab037c
mrbiglari <reza.biglari@sky.uk>
2025-03-04 13:35:32 +0000

    docs: Adding comments for unbuffered channel example in channels tutorial.

--- tutorial/31-channels.go
+++ tutorial/31-channels.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func channels() {
-	var messages = make(chan string)
+	var messages = make(chan string) // an unbuffered channel (make(chan T)) requires both the sender and receiver to be ready at the same time; the sender blocks until the receiver reads the value, ensuring strict synchronization.
 
-	go func() { messages <- "ping" }()
+	go func() { 
+		messages <- "ping" /* blocks here until a receiver reads from the channel */ }()
 
-	var ping = <-messages
+	var ping = <-messages // is blocked until a sender sends a value into the channel
 
 	fmt.Println(ping)
 }
