e3e7483079e54d2e19102a33ae33d7dd49643e2d
mrbiglari <reza.biglari@sky.uk>
2025-03-04 14:57:18 +0000

    docs: Adding comments explaining unbuffered and buffered channel behaviour in channel buffering tutorial.

--- tutorial/32-channel-buffering.go
+++ tutorial/32-channel-buffering.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+/*
+✔ Unbuffered channel (make(chan T)):
+   ● It checks if a receiver is ready.
+   ● If no receiver is ready, it blocks until one is.
+   ● If a receiver is ready, it sends immediately without using any buffer.
+✔ Buffered channel (make(chan T, N)):
+   ● It checks if there is space in the buffer.
+   ● If the buffer is not full, it places the value inside the buffer without blocking.
+   ● If the buffer is full, it blocks until space is available.
+*/
+
 func channelBuffering() {
 	var channel = make(chan string, 2) // a buffered channel (make(chan T, N)) allows the sender to send up to N values without blocking, only blocking if the buffer is full, and the receiver only blocks when the buffer is empty.
 	channel <- "first"
