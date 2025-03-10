a259475566988f46df7d42b7b5d1040838e59cd4
mrbiglari <reza.biglari@sky.uk>
2025-03-10 13:19:40 +0000

    docs: Correcting comments on printed value of 'x' in pointers tutorial.

--- tutorial/17-pointers.go
+++ tutorial/17-pointers.go
@@ -12,12 +12,12 @@ func setZeroValueViaPointer(variable *int) { // `*` in `*int` defines the type o
 
 func pointers() {
 	var x = 5                                 // type of `x` is `int`
-	fmt.Printf("Value of 'x' is: %d\n", x)    // 3
+	fmt.Printf("Value of 'x' is: %d\n", x)    // 5
 	fmt.Printf("Address of 'x' is: %d\n", &x) // 0x42131100
 
 	var y = &x                              // `&` operator gets `address of` `x` // type of `y` is `*int`
 	fmt.Printf("Value of 'y' is: %d\n", y)  // 0x42131100 // the value is the address of `x`.
-	fmt.Printf("Value of 'x' is: %d\n", *y) // 3 // `*` operator `dereferences` pointer `y` to get value of `x`.
+	fmt.Printf("Value of 'x' is: %d\n", *y) // 5 // `*` operator `dereferences` pointer `y` to get value of `x`.
 
 	*y = 10 // dereferencing `y` allows to update value of `x`
 	fmt.Printf("Value of 'x' is now: %d\n", x)
