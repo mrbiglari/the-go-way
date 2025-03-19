cb2aa8bf41a76a58eaf5ae6995ef7f4ae0aff568
Reza-Biglari-qy6_nbcuni <reza.biglari@sky.uk>
2025-03-19 17:37:15 +0000

    feat: add example of chunking slices using slices.Chunk in range-over-built-in-types tutorial

--- tutorial/16-range-over-built-in-types.go
+++ tutorial/16-range-over-built-in-types.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func rangeOverBuiltInTypes() {
 	numbers := []int{2, 3, 4}
 	for index, number := range numbers {
 		fmt.Printf("number[%d]:%d\n", index, number)
+	}
+
+	chunkMe := []string{"It", "is", "possible", "to", "range", "over", "chunks", "of", "a", "slice", "using", "Chunk()"}
+	for chunk := range slices.Chunk(chunkMe, 3) {
+		fmt.Println(chunk)
 	}
 
 	aliases := map[string]string{"apollo13": "bob", "artemis": "sydney"}
