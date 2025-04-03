19c0fe3ebb63ec01343e82bb0ab641406a4d980d
mrbiglari <mrbiglari3000@outlook.com>
2025-04-03 17:33:54 +0100

    feat: add SQS message processing with concurrent and polled reading methods

--- tutorial/96-sqs.go
+++ tutorial/96-sqs.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -11,6 +13,24 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
+
+type TestMessage struct {
+	Message string `json:"Message"`
+}
+
+func extractMessage(msg types.Message) string {
+	var m TestMessage
+	err := json.Unmarshal([]byte(*msg.Body), &m)
+	if err != nil {
+		fmt.Print(err)
+	}
+	message := m.Message
+	return message
+}
+
+func printMessage(msg types.Message) {
+	fmt.Println(extractMessage(msg))
+}
 
 func getConfig() aws.Config {
 	cfg, _ := config.LoadDefaultConfig(context.Background(),
@@ -23,15 +43,45 @@ func getConfig() aws.Config {
 	return cfg
 }
 
-func _sqs() {
-	cfg := getConfig()
-	client := sqs.NewFromConfig(cfg)
-	const queueURL = "http://localhost:4566/000000000000/liveMetricsV3"
+var cfg = getConfig()
+var client = sqs.NewFromConfig(cfg)
+var ctx = context.Background()
 
+const queueURL = "http://localhost:4566/000000000000/queue1"
+
+func _sqs() {
+	readConcurrent() // change to read(), readConcurrent() or readPolled()
+}
+
+func read() {
 	for {
-		output, err := client.ReceiveMessage(context.Background(), &sqs.ReceiveMessageInput{
+		output, err := client.ReceiveMessage(ctx, &sqs.ReceiveMessageInput{
 			QueueUrl:            aws.String(queueURL), // url of the sqs queue to receive messages from
-			MaxNumberOfMessages: 1,                    // maximum number of messages to retrieve (up to 10)
+			MaxNumberOfMessages: 5,                    // maximum number of messages to retrieve (up to 10)
+			VisibilityTimeout:   30,                   // time in seconds the message stays invisible to others after being received
+			WaitTimeSeconds:     20,                   // enable long polling to wait up to 20 seconds for a message
+		})
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, msg := range output.Messages {
+			printMessage(msg) // ...message processing logic
+			client.DeleteMessage(ctx, &sqs.DeleteMessageInput{
+				QueueUrl:      aws.String(queueURL),
+				ReceiptHandle: msg.ReceiptHandle,
+			})
+		}
+	}
+}
+
+func readConcurrent() {
+	for {
+		output, err := client.ReceiveMessage(ctx, &sqs.ReceiveMessageInput{
+			QueueUrl:            aws.String(queueURL), // url of the sqs queue to receive messages from
+			MaxNumberOfMessages: 5,                    // maximum number of messages to retrieve (up to 10)
 			VisibilityTimeout:   30,                   // time in seconds the message stays invisible to others after being received
 			WaitTimeSeconds:     20,                   // enable long polling to wait up to 20 seconds for a message
 		})
@@ -46,13 +96,40 @@ func _sqs() {
 			wg.Add(1)
 			go func(m types.Message) {
 				defer wg.Done()
-				fmt.Println(*m.Body) // ...message processing logic
-				client.DeleteMessage(context.Background(), &sqs.DeleteMessageInput{
+				printMessage(msg) // ...message processing logic
+				client.DeleteMessage(ctx, &sqs.DeleteMessageInput{
 					QueueUrl:      aws.String(queueURL),
 					ReceiptHandle: m.ReceiptHandle,
 				})
 			}(msg)
 		}
 		wg.Wait()
+	}
+}
+
+func readPolled() {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		output, err := client.ReceiveMessage(ctx, &sqs.ReceiveMessageInput{
+			QueueUrl:            aws.String(queueURL), // url of the sqs queue to receive messages from
+			MaxNumberOfMessages: 5,                    // maximum number of messages to retrieve (up to 10)
+			VisibilityTimeout:   20,                   // time in seconds the message stays invisible to others after being received
+			WaitTimeSeconds:     20,                   // enable long polling to wait up to 20 seconds for a message
+		})
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, msg := range output.Messages {
+			printMessage(msg) // ...message processing logic
+			client.DeleteMessage(ctx, &sqs.DeleteMessageInput{
+				QueueUrl:      aws.String(queueURL),
+				ReceiptHandle: msg.ReceiptHandle,
+			})
+		}
 	}
 }
